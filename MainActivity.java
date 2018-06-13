813bf04f2889b91f628d37b652e08e1c33a77237
Segal Au <9510917+SegalAu@users.noreply.github.com>
2018-06-13 00:54:07 -0700

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -3,6 +3,7 @@ package com.example.auseg.navigationcompat;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -20,28 +21,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Declaration of buttons
         toggle = findViewById(R.id.toggle);
         toggleHeadsUp = findViewById(R.id.headsUp);
 
+        //Intent and PendingIntent declarations for the 2 activities progressing from the notifications
         Intent intent = new Intent(MainActivity.this, EmptyActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0 , intent, 0);
 
         Intent intent2 = new Intent(MainActivity.this, EmptyActivity2.class);
         PendingIntent pendingIntent2 = PendingIntent.getActivity(getApplicationContext(), 0, intent2, 0);
 
+
+        //Notification manager and NotificationCompat declarations
         final NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         final NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
         final NotificationCompat.Builder mBuilder2 = new NotificationCompat.Builder(this);
+        //Settings for default notification
         mBuilder
-                .setSmallIcon(R.drawable.notification_icon)
+                .setSmallIcon(R.drawable.ic_action_name)
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(),
+                        R.drawable.skull))
                 .setContentTitle("EMERGENCY: MISSILE INBOUND")
 //                .setContentText("Our sensors have detected an incoming missile. Seek shelter immediately. This is NOT  a drill.")
                 .setStyle(new NotificationCompat.BigTextStyle()
                         .bigText("Our sensors have detected an incoming missile. Seek shelter immediately. This is NOT  a drill."))
                 .setContentIntent(pendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        //Settings for popup notification
         mBuilder2
-                .setSmallIcon(R.drawable.notification_icon)
+                .setSmallIcon(R.drawable.ic_action_name)
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(),
+                        R.drawable.skull))
                 .setContentTitle("EMERGENCY: MISSILE INBOUND")
 //                .setContentText("Our sensors have detected an incoming missile. Seek shelter immediately. This is NOT  a drill.")
                 .setStyle(new NotificationCompat.BigTextStyle()
@@ -54,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
+        //OnclickListener for default notification handling button
         toggle.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -64,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        //OnClickListener for popup notification handling button
         toggleHeadsUp.setOnClickListener(new View.OnClickListener(){
 
             @Override
